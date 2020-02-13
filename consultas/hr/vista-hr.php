851ead0af0cd93473a6c91a0212e533a9a2fcc70
mk <maikolleon@hotmail.com>
2020-02-13 17:13:24 -0500

    orden de codigo

--- consultas/hr/vista-hr.php
+++ consultas/hr/vista-hr.php
@@ -8,7 +8,7 @@
 
     <!-- CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="../../css/estilos.css">
 </head>
 <body>
     <div class="container">
@@ -41,7 +41,7 @@
         <div class="row">
             <div class="col-12">
                 <div id="contHR">
-                    <img src="img/hr.png" alt="HR" class="img img-fluid">
+                    <img src="../../img/hr.png" alt="HR" class="img img-fluid">
                     <div class="items"></div>
                 </div>
             </div>
@@ -50,7 +50,7 @@
 <!-- JavaScript -->
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="funciones.js"></script>
+    <script src="../../js/funciones.js"></script>
 </body>
 </html>
 
