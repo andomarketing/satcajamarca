36f1d4ea94148aa66b896391821cfdd533f60b4d
mk <maikolleon@hotmail.com>
2020-02-13 14:30:45 -0500

    config base datos

--- php/db.php
+++ php/db.php
@@ -1,4 +1,4 @@
 <?php
 
-$mysqli = new mysqli("localhost","root", "", "satcajamarca") or die("Error al Conectar con BD");
+$mysqli = new mysqli("ando.pe","andomarketing_satcajamarca", "Ando2020@", "andomarketing_satcajamarca") or die("Error al Conectar con BD");
 mysqli_set_charset($mysqli,"utf8");
