f7528897cc1e686a278c52cf002aa78d982ea31d
mk <maikolleon@hotmail.com>
2020-02-13 14:32:19 -0500

    config db

--- php/db.php
+++ php/db.php
@@ -2,3 +2,4 @@
 
 $mysqli = new mysqli("ando.pe","andomarketing_satcajamarca", "Ando2020@", "andomarketing_satcajamarca") or die("Error al Conectar con BD");
 mysqli_set_charset($mysqli,"utf8");
+// hola
