83c6befe3db9a453bc4c3bdecbde318acfe8fb78
eduardvives74 <dev.eduardvives@gmail.com>
2020-02-17 10:11:32 -0500

    bd

--- php/db.php
+++ php/db.php
@@ -1,4 +1,4 @@
 <?php
 
-$mysqli =  mysqli_connect("ando.pe","andomarketing_satcajamarca", "Ando2020@", "andomarketing_satcajamarca") or die("Error al Conectar con BD");
+$mysqli =  mysqli_connect("ando.pe","andomarketing_sat2020", "Ando2020@", "andomarketing_sat2020") or die("Error al Conectar con BD");
 mysqli_set_charset($mysqli,"utf8");
