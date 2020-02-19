2cdd29e9a008aee8f0fc523e50a4fc9441c678d9
eduardvives74 <dev.eduardvives@gmail.com>
2020-02-18 19:19:18 -0500

    maqueta pu

--- consultas/pu/vista-pu.php
+++ consultas/pu/vista-pu.php
@@ -47,6 +47,50 @@
             </div>
         </div>
     </div>
+
+
+    <!-- TABLA -->
+
+    <table class="table table-bordered tabla_pu" style="background-image:  url('../../img/pu.png');
+    background-size: cover;
+background-repeat: no-repeat;
+    " >
+	<tbody>
+		<tr style="height: 113px;">
+			<td>  <p class="emular">LOGO SAT </p></td>
+			<td>  <p class="emular"> PU </p></td>
+			
+		</tr>
+		<tr>
+            <td class="center" colspan="2"> <p class="emular"> DECLARACION JUARADA MECANIZADA DEL IMPUESTO PREDIAL </p>
+            <p class="emular"> XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX </p>
+            <p class="emular">DECLARACION JUARADA N <span class="dato">54554</span>  </p>
+            </td>
+			
+		</tr >
+		<tr>
+			<td><p class="emular dato_predio_pu">Datos del predio: <span class="dato">XXXXX </span></p></td>
+			<td><p class="emular"> emision: <span class="dato emision_pu">445333  </span></p></td>
+		
+		</tr>
+		<tr>
+			<td colspan="2">
+                 <p class="emular"> CODIGO CONTRIBUYENTE:<span class="dato codigo_contribuyente_pu">31684</span></p>
+                 <p class="emular"> NOMBRE: <span class="dato nombre_pu">EL WAYNER</span></p>
+                 <p class="emular"> CODIGO PREDIO: <span class="dato codigo_predio_pu">23344</span></p>
+                 <p class="emular"> DIRECCION DEL PREDIOO:<span class="dato direccion_predio_pu">AV Arequipa centro comercial la casnona 134</span></p>
+                 <p class="emular"> LUGAR: <span class="dato lugar_pu">xxxx</span></p>
+                 <p class="emular"> SECTOR: <span class="dato sector_pu">AS</span></p>
+                
+            </td>
+			
+			
+        </tr>
+        
+      
+	
+	</tbody>
+</table>
 <!-- JavaScript -->
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
