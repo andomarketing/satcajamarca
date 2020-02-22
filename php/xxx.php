c99e018792f6946364d1183b8d68d1c0880ef124
andomarketing <holaando@gmail.com>
2020-02-22 06:18:32 -0500

    ready-for-tests

--- php/xxx.php
+++ php/xxx.php
@@ -6,13 +6,13 @@
     <title>Document</title>
     <style>
         body {
-            margin-left: 60px;
-            margin-top: 30px;
+            margin-top: 36.5mm; 
+            margin-left: 15mm;
         }
         table {            
             border-spacing: 0px;
             width: 129mm;
-            border: 0.005mm solid black
+            border: 0.005mm solid blue;
         }
         .white-text {
             font-weight: 100;
@@ -44,14 +44,15 @@
         .pre3-table {
             font-size: 5pt;
             border: none;
-            margin-bottom: 6mm;
+            margin-bottom: 7.5mm;
         }
         .pre3-td {
             padding-left: 1.5mm;
         }
-        .pre4-td {
+        .pre4-table{
             font-size: 4pt;
             height: 6mm;
+            margin-bottom: 6mm;
         }
         .pre4-td-left {
             padding-left: 1mm;
@@ -60,10 +61,35 @@
         .pre4-td-rigth {
             width:31%;
         }
+        .vertical-text {
+            writing-mode: vertical-lr;
+            transform: rotate(180deg);
+        }
+        .horizontal-text {
+            font-size: 3pt;
+        }
+        .const-table {
+            font-size: 4pt;
+            text-align: center;
+            vertical-align: bottom;
+            margin-bottom: 8mm;
+        }
+        .const-table td{
+            border-right: 0.005mm solid blue;
+            border-bottom: 0.005mm solid blue;
+        }
+        .comp-table {
+            font-size: 4pt;
+            text-align: center;
+        }
+        .comp-table td{
+            border-right: 0.005mm solid blue;
+            border-bottom: 0.005mm solid blue;
+        }
     </style>
 </head>
 <body>
-<div style="display: block; width: 100% ; height: 370mm; position:relative; font-size: 6pt; font-family: Arial; font-weight: bold; page-break-after: always;">
+<div style="display: block; width: 100% ; height: 155mm; font-size: 6pt; font-family: Arial; font-weight: bold; page-break-after: always;">
     
     <table class="pre1-table">
         <tr class="pre1-tr">
@@ -112,14 +138,116 @@
         </tr>
     </table>
     
-    <table class="construccion-table">
-        <tr style="height: 23mm;">
-            <td style="border: 0.01mm solid black"></td>
+    <table class="const-table">
+        <tr style="height: 23mm">
+            <td style="width: 5mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="vertical-text white-text">TN</span></td>
+            <td style="width: 4mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="vertical-text white-text">NN</span></td>
+            <td style="width: 4mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="vertical-text white-text">SECCIÓN</span></td>
+            <td style="width: 3.5mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="vertical-text white-text">ANTIGUEDAD</span></td>
+            <td style="width: 4mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="vertical-text white-text">MP</span></td>
+            <td style="width: 4mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="vertical-text white-text">EDC</span></td>
+            <td style="width: 3.5mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="horizontal-text white-text">MC</span></td>
+            <td style="width: 4mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="horizontal-text white-text">T</span></td>
+            <td style="width: 4mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="horizontal-text white-text">P</span></td>
+            <td style="width: 4mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="horizontal-text white-text">PV</span></td>
+            <td style="width: 3.5mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="horizontal-text white-text">RV</span></td>
+            <td style="width: 4mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="horizontal-text white-text">B</span></td>
+            <td style="width: 4mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="horizontal-text white-text">IE</span></td>
+            <td style="width: 7mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="vertical-text white-text">VUPM2</span></td>
+            <td style="width: 6mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="vertical-text white-text">ID5%</span></td>
+            <td style="width: 9mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="horizontal-text white-text">%</span></td>
+            <td style="width: 10mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="horizontal-text white-text">VALOR</span></td>
+            <td style="width: 9mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="vertical-text white-text">VUD</span></td>
+            <td style="width: 8.8mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="vertical-text white-text">ÁC</span></td>
+            <td style="width: 12.5mm; vertical-align: bottom; padding-bottom: 2mm;"><span class="vertical-text white-text">ÁCM2</span></td>
+            <td style="width: 18mm; vertical-align: bottom; padding-bottom: 2mm;border-bottom: 0.01mm solid blue"><span class="vertical-text white-text">VDLC</span></td>
         </tr>
         <tr style="height: 27.5mm">
-            <td style="border-left: 0.01mm solid black; border-bottom: 0.01mm solid black; border-right: 0.01mm solid black;"></td>
+        <td><span class="vertical-text"></span></td>
+            <td><span class="vertical-text"></span></td>
+            <td><span class="vertical-text"></span></td>
+            <td><span class="vertical-text"></span></td>
+            <td><span class="vertical-text"></span></td>
+            <td><span class="vertical-text"></span></td>
+            <td><span class="horizontal-text">MC</span></td>
+            <td><span class="horizontal-text">T</span></td>
+            <td><span class="horizontal-text">P</span></td>
+            <td><span class="horizontal-text">PV</span></td>
+            <td><span class="horizontal-text">RV</span></td>
+            <td><span class="horizontal-text">B</span></td>
+            <td><span class="horizontal-text">IE</span></td>
+            <td><span class="vertical-text"></span></td>
+            <td><span class="vertical-text"></span></td>
+            <td><span class="horizontal-text">%</span></td>
+            <td><span class="horizontal-text"></span></td>
+            <td><span class="vertical-text"></span></td>
+            <td><span class="vertical-text"></span></td>
+            <td><span class="vertical-text"></span></td>
+            <td><span class="vertical-text"></span></td>
         </tr>
-        <tr></tr>
+        <tr style="height: 5mm">
+            <td colspan="15">
+            </td>
+            <td colspan="2">
+            </td>
+            <td colspan="2">
+            </td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+
+    <table class="comp-table">
+        <tr style="height: 6mm">
+            <td style="width: 8mm; font-size: 3pt">Item</td>
+            <td style="width: 8mm; font-size: 3pt">Código</td>
+            <td style="width: 31mm; font-size: 3pt">TI</td>
+            <td style="width: 16mm; font-size: 3pt">AI</td>
+            <td style="width: 10mm; font-size: 3pt">Medida</td>
+            <td style="width: 9mm; font-size: 3pt">Tipo</td>
+            <td style="width: 10mm; font-size: 3pt">VU</td>
+            <td style="width: 13mm; font-size: 3pt">VI</td>
+            <td style="width: 11mm; font-size: 3pt">VO</td>
+            <td style="width: 11mm; font-size: 3pt">Valor Total</td>
+        </tr>
+        <tr style="height: 15mm">
+            <td>Item</td>
+            <td>Código</td>
+            <td>Tipo Instalación</td>
+            <td>Año Instalación</td>
+            <td>Medida</td>
+            <td>Tipo</td>
+            <td>Valor Unitario</td>
+            <td>Valor Instalación</td>
+            <td>Valor Oficialzación</td>
+            <td>Valor Total</td>
+        </tr>
+        <tr style="height: 2mm">
+            <td colspan="10"></td>
+        </tr>
+        <tr style="height: 3mm">
+            <td colspan="10"></td>
+        </tr>
+        <tr style="height: 3.666666666666667mm">
+            <td colspan="4"></td>
+            <td colspan="4" style="font-size: 3pt; text-align: left; padding: 1mm">VALOR DE LA CONSTRUCCIÓN</td>
+            <td colspan="2"></td>
+        </tr>
+        <tr style="height: 3.666666666666667mm">
+            <td colspan="4"></td>
+            <td colspan="4" style="font-size: 3pt; text-align: left; padding: 1mm">VALOR DE LA CONSTRUCCIÓN</td>
+            <td colspan="2"></td>
+        </tr>
+        <tr style="height: 3.666666666666667mm">
+            <td colspan="4"></td>
+            <td colspan="4" style="font-size: 3pt; text-align: left; padding: 1mm">VALOR DE LA CONSTRUCCIÓN</td>
+            <td colspan="2"></td>
+        </tr>
+        <tr style="height: 3.3mm">
+            <td colspan="4"></td>
+            <td colspan="4" style="font-size: 3pt; text-align: left; padding: 1mm">VALOR DE LA CONSTRUCCIÓN</td>
+            <td colspan="2"></td>
+        </tr>
     </table>
 </div>
 </body>
