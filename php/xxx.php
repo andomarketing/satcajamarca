cf79030b3a974dce0081456f8aa90c043ceb3979
andomarketing <holaando@gmail.com>
2020-02-22 03:20:57 -0500

    4 tablas listas

--- php/xxx.php
+++ php/xxx.php
@@ -4,68 +4,124 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            margin-left: 60px;
+            margin-top: 30px;
+        }
+        table {            
+            border-spacing: 0px;
+            width: 129mm;
+            border: 0.005mm solid black
+        }
+        .white-text {
+            font-weight: 100;
+        }
+        .pre1-table {
+            font-size: 6pt;
+            height: 18mm;
+            margin-bottom: 1mm;
+        }
+        .pre1-td {
+            padding-left: 3mm;
+        }
+        .pre1-td-left {}
+        .pre1-td-rigth {
+            width: 57.4%;
+        }
+        .pre2-table {
+            font-size: 5pt;
+            height: 10mm;
+            margin-bottom: 1mm;
+        }
+        .pre2-td-left {
+            padding-left: 3mm;
+            width: 37.1%;
+        }
+        .pre2-td-rigth {
+            width:30%;
+        }
+        .pre3-table {
+            font-size: 5pt;
+            border: none;
+            margin-bottom: 6mm;
+        }
+        .pre3-td {
+            padding-left: 1.5mm;
+        }
+        .pre4-td {
+            font-size: 4pt;
+            height: 6mm;
+        }
+        .pre4-td-left {
+            padding-left: 1mm;
+            width: 36%;
+        }
+        .pre4-td-rigth {
+            width:31%;
+        }
+    </style>
 </head>
 <body>
-    <div class="book">
-        <div class="page">
-            <div class="subpage">Page 1/2</div>    
-        </div>
-        <div class="page">
-            <div class="subpage">Page 2/2</div>    
-        </div>
-    </div>
-
-    <style>
-            body {
-        width: 100%;
-        height: 100%;
-        margin: 0;
-        padding: 0;
-        background-color: #FAFAFA;
-        font: 12pt "Tahoma";
-    }
-    * {
-        box-sizing: border-box;
-        -moz-box-sizing: border-box;
-    }
-    .page {
-        width: 210mm;
-        min-height: 297mm;
-        padding: 20mm;
-        margin: 10mm auto;
-        border: 1px #D3D3D3 solid;
-        border-radius: 5px;
-        background: white;
-        box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-    }
-    .subpage {
-        padding: 1cm;
-        border: 5px red solid;
-        height: 257mm;
-        outline: 2cm #FFEAEA solid;
-    }
+<div style="display: block; width: 100% ; height: 370mm; position:relative; font-size: 6pt; font-family: Arial; font-weight: bold; page-break-after: always;">
     
-    @page {
-        size: A4;
-        margin: 0;
-    }
-    @media print {
-        html, body {
-            width: 210mm;
-            height: 297mm;        
-        }
-        .page {
-            margin: 0;
-            border: initial;
-            border-radius: initial;
-            width: initial;
-            min-height: initial;
-            box-shadow: initial;
-            background: initial;
-            page-break-after: always;
-        }
-    }
-    </style>
+    <table class="pre1-table">
+        <tr class="pre1-tr">
+            <td class="pre1-td pre1-td-left"><span class="white-text">CÓDIGO CONTRIBUYENTE:</span></td>
+            <td class="pre1-td pre1-td-rigth"></td>
+        </tr>
+        <tr class="pre1-tr">
+            <td class="pre1-td pre1-td-left"><span class="white-text">NOMBRE:</span></td>
+            <td class="pre1-td pre1-td-rigth"></td>
+        </tr>
+        <tr class="pre1-tr">
+            <td class="pre1-td pre1-td-left"><span class="white-text">CÓDIGO PREDIO:</span></td>
+            <td class="pre1-td pre1-td-rigth"><span class="white-text">CÓDIGO CATASTRAL:</span></td>
+        </tr>
+        <tr class="pre1-tr">
+            <td class="pre1-td pre1-td-left"><span class="white-text">DIRECCIÓN DE PREDIO:</span></td>
+            <td class="pre1-td pre1-td-rigth"></td>
+        </tr>
+    </table>
+
+    <table class="pre2-table">
+        <tr class="pre2-tr">
+            <td class="pre2-td pre2-td-left"><span class="white-text">COND. PROPIEDAD:</span></td>
+            <td class="pre2-td pre2-td-center"><span class="white-text">% DE PARTICIPACIÓN:</span></td>
+            <td class="pre2-td pre2-td-rigth"><span class="white-text">TIPO DE TIERRA:</span></td>
+        </tr>
+        <tr class="pre2-tr">
+            <td class="pre2-td pre2-td-left"><span class="white-text">ALTITUD:</span></td>
+            <td class="pre2-td pre2-td-center"><span class="white-text">CATEGORIA:</span></td>
+            <td class="pre2-td pre2-td-rigth"><span class="white-text">USO:</span></td>
+        </tr>
+    </table>
+    <table class="pre3-table">
+        <tr>
+            <td class="pre3-td">
+                <span class="white-text">CONDICIÓN DE PROPIEDAD:</span>
+            </td>
+        </tr>
+    </table>    
+
+    <table class="pre4-table">
+        <tr class="pre4-tr">
+            <td class="pre4-td pre4-td-left"><span class="white-text">AREA DEL TERRENO HA:</span></td>
+            <td class="pre4-td pre4-td-center"><span class="white-text">VALOR ARANCELARIO:</span></td>
+            <td class="pre4-td pre4-td-rigth"><span class="white-text">VALOR TERNARIO:</span></td>
+        </tr>
+    </table>
+    
+    <table class="construccion-table">
+        <tr style="height: 23mm;">
+            <td style="border: 0.01mm solid black"></td>
+        </tr>
+        <tr style="height: 27.5mm">
+            <td style="border-left: 0.01mm solid black; border-bottom: 0.01mm solid black; border-right: 0.01mm solid black;"></td>
+        </tr>
+        <tr></tr>
+    </table>
+</div>
 </body>
 </html>
 
