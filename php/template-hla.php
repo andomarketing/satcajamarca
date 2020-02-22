828398b756669d4f799879e5e2121092fa870535
andomarketing <holaando@gmail.com>
2020-02-22 08:55:26 -0500

    ready-for-tests

---
+++ php/template-hla.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            margin-top: 34mm; 
+            margin-left: 15mm;
+        }
+        table {            
+            border-spacing: 0px;
+            width: 129mm;
+            border: 0.005mm solid blue;
+        }
+        .white-text {
+            font-weight: 100;
+        }
+        .pre1-table {
+            padding-top: 2mm;
+            font-size: 6pt;
+            height: 23mm;
+            margin-bottom: 18mm;
+        }
+        .pre1-td {
+            padding-left: 3mm;
+        }
+        .pre1-td-left {}
+        .pre1-td-rigth {
+            width: 57.4%;
+        }
+        .pre2-table {
+            padding-top: 1mm;
+            font-size: 6pt;
+            height: 26mm;
+            margin-bottom: 6mm;
+        }
+        .pre2-table td {
+            padding-left: 0mm;
+        }
+        .pre2-td-left {            
+            width: 54mm;
+        }
+        .pre2-td-rigth {            
+            width: 76mm;
+        }
+        .pre3-table {
+            padding-top: 0.5mm;
+            font-size: 6pt;
+            height: 24.5mm;
+            margin-bottom: 5mm;
+        }
+        .pre3-table td {
+            padding-left: 0mm;
+        }
+        .pre3-td-left {            
+            width: 54mm;
+        }
+        .pre3-td-rigth {            
+            width: 76mm;
+        }
+        .pre4-table {
+            padding-top: 1mm;
+            font-size: 6pt;
+            height: 17.5mm;
+            margin-bottom: 6.5mm;
+        }
+        .pre4-table td {
+            padding-left: 0mm;
+        }
+        .pre4-td-left {            
+            width: 54mm;
+        }
+        .pre4-td-rigth {            
+            width: 76mm;
+        }
+        .pre5-table {
+            padding-top: 0mm;
+            font-size: 6pt;
+            height: 18mm;
+            margin-bottom: 6.5mm;
+        }
+        .pre5-table td {
+            padding-left: 0mm;
+        }
+        .pre5-td-left {            
+            width: 54mm;
+        }
+        .pre5-td-rigth {            
+            width: 76mm;
+        }
+    </style>
+</head>
+<body>
+<div style="display: block; width: 100% ; height: 155mm; font-size: 6pt; font-family: Arial; font-weight: bold; page-break-after: always;">
+    
+    <table class="pre1-table">
+        <tr class="pre1-tr">
+            <td class="pre1-td pre1-td-left"><span class="white-text">CÓDIGO CONTRIBUYENTE</span></td>
+            <td class="pre1-td pre1-td-rigth"></td>
+        </tr>
+        <tr class="pre1-tr">
+            <td class="pre1-td pre1-td-left"><span class="white-text">NOMBRE</span></td>
+            <td class="pre1-td pre1-td-rigth"></td>
+        </tr>
+        <tr class="pre1-tr">
+            <td class="pre1-td pre1-td-left"><span class="white-text">CÓDIGO PREDIO</span></td>
+            <td class="pre1-td pre1-td-rigth"></td>
+        </tr>
+        <tr class="pre1-tr">
+            <td class="pre1-td pre1-td-left"><span class="white-text">DIRECCIÓN DE PREDIO</span></td>
+            <td class="pre1-td pre1-td-rigth"></td>
+        </tr>
+    </table>
+
+    <table class="pre2-table">
+        <tr class="pre2-tr">
+            <td class="pre2-td pre2-td-left"><span class="white-text">Frecuencia de barrido de calles</span></td>
+            <td class="pre2-td pre2-td-rigth"><span class="white-text">Diaria</span></td>
+        </tr>
+        <tr class="pre2-tr">
+            <td class="pre2-td pre2-td-left"><span class="white-text">Longitud determinada por</span></td>
+            <td class="pre2-td pre2-td-rigth"><span class="white-text">Metros lineales</span></td>
+        </tr>
+        <tr class="pre2-tr">
+            <td class="pre2-td pre2-td-left"><span class="white-text">Longitud de frontis</span></td>
+            <td class="pre2-td pre2-td-rigth"><span class="white-text">(a)</span></td>
+        </tr>
+        <tr class="pre2-tr">
+            <td class="pre2-td pre2-td-left"><span class="white-text">*Costo anual por metros lineales</span></td>
+            <td class="pre2-td pre2-td-rigth"><span class="white-text">(b)</span></td>
+        </tr>
+        <tr class="pre2-tr">
+            <td class="pre2-td pre2-td-left"><span class="white-text">*Monto real anual</span></td>
+            <td class="pre2-td pre2-td-rigth"><span class="white-text">(c) = (a) x (b)</span></td>
+        </tr>
+        <tr class="pre2-tr">
+            <td class="pre2-td pre2-td-left"><span class="white-text">Monto subvencionado anual</span></td>
+            <td class="pre2-td pre2-td-rigth"><span class="white-text">(d) = (c) x 0.2</span></td>
+        </tr>
+        <tr class="pre2-tr">
+            <td class="pre2-td pre2-td-left"><span class="white-text">Tasa trimestral 2020</span></td>
+            <td class="pre2-td pre2-td-rigth"><span class="white-text">(e) = [(c) - (d)]/4</span></td>
+        </tr>
+    </table>
+    <table class="pre3-table">
+        <tr class="pre3-tr">
+            <td class="pre3-td pre3-td-left"><span class="white-text">*Habitantes Promedio</span></td>
+            <td class="pre3-td pre3-td-rigth"><span class="white-text">3</span></td>
+        </tr>
+        <tr class="pre3-tr">
+            <td class="pre3-td pre3-td-left"><span class="white-text">Área promedio /Área utilizada m2</span></td>
+            <td class="pre3-td pre3-td-rigth"><span class="white-text">(a)</span></td>
+        </tr>
+        <tr class="pre3-tr">
+            <td class="pre3-td pre3-td-left"><span class="white-text">Uso del predio</span></td>
+            <td class="pre3-td pre3-td-rigth"><span class="white-text"></span></td>
+        </tr>
+        <tr class="pre3-tr">
+            <td class="pre3-td pre3-td-left"><span class="white-text">*Costo anual por m2</span></td>
+            <td class="pre3-td pre3-td-rigth"><span class="white-text">(b)</span></td>
+        </tr>
+        <tr class="pre3-tr">
+            <td class="pre3-td pre3-td-left"><span class="white-text">*Monto real anual</span></td>
+            <td class="pre3-td pre3-td-rigth"><span class="white-text">(c) = (a) x (b)</span></td>
+        </tr>
+        <tr class="pre3-tr">
+            <td class="pre3-td pre3-td-left"><span class="white-text">Monto subvencionado anual</span></td>
+            <td class="pre3-td pre3-td-rigth"><span class="white-text">(d) = (c) x 0.2</span></td>
+        </tr>
+        <tr class="pre3-tr">
+            <td class="pre3-td pre3-td-left"><span class="white-text">Tasa trimestral 2020</span></td>
+            <td class="pre3-td pre3-td-rigth"><span class="white-text">(e) = [(c) - (d)]/4</span></td>
+        </tr>
+    </table>
+    <table class="pre4-table">
+        <tr class="pre4-tr">
+            <td class="pre4-td pre4-td-left"><span class="white-text">Descripción de la ubicación</span></td>
+            <td class="pre4-td pre4-td-rigth"><span class="white-text"></span></td>
+        </tr>
+        <tr class="pre4-tr">
+            <td class="pre4-td pre4-td-left"><span class="white-text">*Índice de disfrute</span></td>
+            <td class="pre4-td pre4-td-rigth"><span class="white-text"></span></td>
+        </tr>
+        <tr class="pre4-tr">
+            <td class="pre4-td pre4-td-left"><span class="white-text">*Monto real anual</span></td>
+            <td class="pre4-td pre4-td-rigth"><span class="white-text">(a)</span></td>
+        </tr>
+        <tr class="pre4-tr">
+            <td class="pre4-td pre4-td-left"><span class="white-text">Monto subvencionado anual</span></td>
+            <td class="pre4-td pre4-td-rigth"><span class="white-text"></span></td>
+        </tr>
+        <tr class="pre4-tr">
+            <td class="pre4-td pre4-td-left"><span class="white-text">Tasa trimestral 2020</span></td>
+            <td class="pre4-td pre4-td-rigth"><span class="white-text">(a) - (b) /4</span></td>
+        </tr>
+    </table>
+    <table class="pre5-table">
+        <tr class="pre5-tr">
+            <td class="pre5-td pre5-td-left"><span class="white-text">Zona</span></td>
+            <td class="pre5-td pre5-td-rigth"><span class="white-text"></span></td>
+        </tr>
+        <tr class="pre5-tr">
+            <td class="pre5-td pre5-td-left"><span class="white-text">Uso del predio</span></td>
+            <td class="pre5-td pre5-td-rigth"><span class="white-text"></span></td>
+        </tr>
+        <tr class="pre5-tr">
+            <td class="pre5-td pre5-td-left"><span class="white-text">*Índice de peligrosidad</span></td>
+            <td class="pre5-td pre5-td-rigth"><span class="white-text"></span></td>
+        </tr>
+        <tr class="pre5-tr">
+            <td class="pre5-td pre5-td-left"><span class="white-text">*Monto real anual</span></td>
+            <td class="pre5-td pre5-td-rigth"><span class="white-text">(a)</span></td>
+        </tr>
+        <tr class="pre5-tr">
+            <td class="pre5-td pre5-td-left"><span class="white-text">Tasa trimestral 2020</span></td>
+            <td class="pre5-td pre5-td-rigth"><span class="white-text">(b) = (a) /4</span></td>
+        </tr>
+    </table>
+</div>
+</body>
+</html>
+
