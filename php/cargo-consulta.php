2ea81bbbb35bb5b5fd259a325b855c9451a5dadc
Ando <hola@ando.pe>
2020-02-22 12:59:31 -0500

    ready-to-print

---
+++ php/cargo-consulta.php
@@ -0,0 +1,130 @@
+<?php
+
+function render($template, $param){
+    ob_start();
+    extract($param, EXTR_OVERWRITE);
+    include($template);
+    return ob_get_clean();
+}
+
+define("CARGO_TEMPLATE", "template-cargo.php");
+
+$offset = $_GET["offset"];
+
+//CONEXION BASE DE DATOS
+include "db.php";
+
+//EXISTE UN ERROR DE CONEXION
+if ($mysqli->connect_errno) {
+    $data = array("error" => true, "valor" => "Error: " . $mysqli->connect_error);
+    echo json_encode($data);
+    exit;
+}
+
+//CONSULTAS SQL
+$contribuyenteSql  = "SELECT * FROM tempo_contribuyentes_2020 ORDER BY persona_id LIMIT $offset, 10";
+
+//FALLO LA CONSULTA SQL
+if (!$consultaContribuyente = $mysqli->query($contribuyenteSql)) {
+    $data = array("error" => true, "valor" => "Error consultando el contributyente: " . $mysqli->error);
+    echo json_encode($data);
+    exit;
+}
+
+//NO SE ENCONTRARON REGISTROS
+if ($consultaContribuyente->num_rows === 0) {
+    $data = array("error" => true, "valor" => "No se han encontrado registros de ese contribuyente.");
+    echo json_encode($data);
+    exit;
+}
+
+//GUARDAR CONSULTA EN ARRAY
+while ($contribuyente = $consultaContribuyente->fetch_array()): 
+    $params = [
+        "declaracionJurada" => $contribuyente["NroDeclaracionJurada"],
+        "emision" => $contribuyente["emision"],
+        "codigoContribuyente" => $contribuyente["persona_id"],
+        "dni" => $contribuyente["nro_docu_identidad"],
+        "nombresYApellidos" => $contribuyente["apellidos_nombres"],
+        "tipoContribuyente" => $contribuyente["tipo_Contribuyente"],
+        "domicilioFiscal" => $contribuyente["domicilio_completo"],
+        "referencia" => $contribuyente["referencia"],
+        "mzUbicacion" => $contribuyente["ManCatastral"],
+    ];
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+        .main {
+            margin-top: 18mm;
+            margin-left: 9mm;
+            font-size: 8pt;
+            max-width: 130mm;
+        }
+        table { 
+            width: 100%;
+            border: 0.05mm solid black;
+        }
+        .white-text {
+            font-weight: 100;
+        }
+        .pre1-table {
+            padding-top: 1mm;
+            padding-left: 0.5mm;
+            font-size: 8pt;
+            font-weight: bold;
+            height: 9mm;
+            margin-bottom: 1.5mm;
+        }
+        .pre1-td-left {
+            width: 87mm;
+        }
+        .pre2-table {
+            padding-top: 0mm;
+            padding-left: 0mm;
+            font-size: 8pt;
+            font-weight: bold;
+            min-height: 13mm;
+            margin-bottom: -2.5mm;
+        }
+        .pre2-td-left {
+            width: 87mm;
+        }
+        .pre3-table {
+            padding-top: 0mm;
+            padding-left: 0mm;
+            font-size: 8pt;
+            font-weight: bold;
+            height: 4mm;
+            margin-bottom: 3mm;
+        }
+        .pre3-td-left {
+            width: 87mm;
+        }
+        .white-text {
+            color: transparent;
+        }
+
+        @media print {
+            table {
+                border: none;
+            }
+        }
+    </style>
+</head>
+<body>
+<?php
+  echo render(CARGO_TEMPLATE, $params);
+?>
+</body>
+</html>
+<?php 
+endwhile;
--- php/template-cargo.php
+++ php/template-cargo.php
@@ -1,86 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        body {
-            margin-top: 18mm;
-            margin-left: 8mm;
-        }
-        table { 
-            width: 130mm;
-            border: 0.005mm solid blue;
-        }
-        .white-text {
-            font-weight: 100;
-        }
-        .pre1-table {
-            padding-top: 1mm;
-            padding-left: 0.5mm;
-            font-size: 8pt;
-            font-weight: bold;
-            height: 9mm;
-            margin-bottom: 2mm;
-        }
-        .pre1-td-left {
-            width: 87mm;
-        }
-        .pre2-table {
-            padding-top: 0.5mm;
-            padding-left: 0mm;
-            font-size: 8pt;
-            font-weight: bold;
-            height: 9mm;
-            margin-bottom: 1mm;
-        }
-        .pre2-td-left {
-            width: 87mm;
-        }
-        .pre3-table {
-            padding-top: 0mm;
-            padding-left: 0mm;
-            font-size: 8pt;
-            font-weight: bold;
-            height: 4mm;
-            margin-bottom: 3mm;
-        }
-        .pre3-td-left {
-            width: 87mm;
-        }
-    </style>
-</head>
-<body>
-<div style="display: block; width: 100% ; height: 155mm; font-size: 6pt; font-family: Arial; font-weight: bold; page-break-after: always;">
+
+<div class="main" style="display: block; width: 100% ; height: 155mm; font-size: 6pt; font-family: Arial; font-weight: bold; page-break-after: always;">
     
     <table class="pre1-table">
         <tr class="pre1-tr">
-            <td class="pre1-td pre1-td-left"><span class="white-text">Código Contribuyente&nbsp&nbsp:</span></td>
-            <td class="pre1-td pre1-td-rigth" style="padding-left: 4.5mm;"><span class="white-text">DNI/RUC&nbsp&nbsp:</span></td>
+            <td class="pre1-td pre1-td-left"><span class="white-text">Código Contribuyente&nbsp&nbsp:</span> <?= $codigoContribuyente ?></td>
+            <td class="pre1-td pre1-td-rigth" style="padding-left: 4.5mm;"><span class="white-text">DNI/RUC&nbsp&nbsp:</span> <?= $dni ?></td>
         </tr>
         <tr class="pre1-tr">
-            <td class="pre1-td pre1-td-left"><span class="white-text">Nombre/Razón Social&nbsp&nbsp:</span></td>
-            <td class="pre1-td pre1-td-rigth"></td>
+            <td class="pre1-td pre1-td-left" colspan="2"><span class="white-text">Nombre/Razón Social&nbsp&nbsp:</span> <?= $nombresYApellidos ?></td>
         </tr>
     </table>
     <table class="pre2-table">
         <tr class="pre2-tr">
-            <td class="pre2-td pre2-td-left"><span class="white-text">Tipo de Contribuyente&nbsp&nbsp:</span></td>
-            <td class="pre2-td pre2-td-rigth"><span class="white-text"></span></td>
+            <td class="pre2-td pre2-td-left" colspan="2"><span class="white-text">Tipo de Contribuyente&nbsp&nbsp:</span> <?= $tipoContribuyente ?></td>
         </tr>
         <tr class="pre2-tr">
-            <td class="pre2-td pre2-td-left"><span class="white-text">Domicilio Fiscal&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp:</span></td>
-            <td class="pre2-td pre2-td-rigth"></td>
+            <td class="pre2-td pre2-td-left" colspan="2" style="position: relative;"><span class="white-text">Domicilio Fiscal&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp:</span><span style="font-size: 6pt; position: absolute; width: 70%; top: -0.5mm; rigth: 2mm"> <?= $domicilioFiscal ?> </span></td>
         </tr>
     </table>
     <table class="pre3-table">
         <tr class="pre3-tr">
-            <td class="pre3-td pre3-td-left"><span class="white-text">Referencia&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp:</span></td>
-            <td class="pre3-td pre3-td-rigth"><span class="white-text">Mz/Ubicación&nbsp&nbsp:</span></td>
+            <td class="pre3-td pre3-td-left"><span class="white-text">Referencia&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp:</span> <?= $referencia ?></td>
+            <td class="pre3-td pre3-td-rigth"><span class="white-text">Mz/Ubicación&nbsp&nbsp:</span> <?= $mzUbicacion ?> </td>
         </tr>
     </table>
 </div>
-</body>
-</html>
-
